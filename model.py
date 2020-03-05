c7374e9e58af0271621ed83e225503f32af21a04
E-STAT <owojori.tolulope@gmail.com>
2020-03-05 18:44:16 +0100

    ..

---
+++ model.py
@@ -0,0 +1,67 @@
+import pandas as pd
+import numpy as np
+%matplotlib inline
+import matplotlib.pyplot as plt
+import matplotlib.lines as mlines
+from mpl_toolkits.mplot3d import Axes3D
+import seaborn as sns
+from sklearn.model_selection import train_test_split, learning_curve
+from sklearn.metrics import average_precision_score
+from xgboost.sklearn import XGBClassifier
+from xgboost import plot_importance, to_graphviz
+import pickle
+
+
+df = pd.read_csv('Fraud_data_kaggle.csv')
+X = df.loc[(df.type == 'TRANSFER') | (df.type == 'CASH_OUT')]
+
+randomState = 5
+np.random.seed(randomState)
+
+#X = X.loc[np.random.choice(X.index, 100000, replace = False)]
+
+Y = X['isFraud']
+del X['isFraud']
+
+# Eliminate columns shown to be irrelevant for analysis in the EDA
+X = X.drop(['nameOrig', 'nameDest', 'isFlaggedFraud'], axis = 1)
+
+# Binary-encoding of labelled data in 'type'
+X.loc[X.type == 'TRANSFER', 'type'] = 0
+X.loc[X.type == 'CASH_OUT', 'type'] = 1
+X.type = X.type.astype(int) # convert dtype('O') to dtype(int)
+
+Xfraud = X.loc[Y == 1]
+XnonFraud = X.loc[Y == 0]
+print('\nThe fraction of fraudulent transactions with \'oldBalanceDest\' = \
+\'newBalanceDest\' = 0 although the transacted \'amount\' is non-zero is: {}'.\
+format(len(Xfraud.loc[(Xfraud.oldbalanceDest == 0) & \
+(Xfraud.newbalanceDest == 0) & (Xfraud.amount)]) / (1.0 * len(Xfraud))))
+
+print('\nThe fraction of genuine transactions with \'oldBalanceDest\' = \
+newBalanceDest\' = 0 although the transacted \'amount\' is non-zero is: {}'.\
+format(len(XnonFraud.loc[(XnonFraud.oldbalanceDest == 0) & \
+(XnonFraud.newbalanceDest == 0) & (XnonFraud.amount)]) / (1.0 * len(XnonFraud))))
+
+X.loc[(X.oldbalanceDest == 0) & (X.newbalanceDest == 0) & (X.amount != 0), \
+      ['oldbalanceDest', 'newbalanceDest']] = - 1
+      
+      
+X.loc[(X.oldbalanceDest == 0) & (X.newbalanceDest == 0) & (X.amount != 0), \
+      ['oldbalanceDest', 'newbalanceDest']] = np.nan      
+
+X['errorBalanceOrig'] = X.oldbalanceOrg + X.amount - X.oldbalanceOrg
+X['errorBalanceDest'] = X.oldbalanceDest + X.amount - X.newbalanceDest
+
+trainX, testX, trainY, testY = train_test_split(X, Y, test_size = 0.2, \
+                                                random_state = randomState)
+
+# Long computation in this cell (~1.8 minutes)
+weights = (Y == 0).sum() / (1.0 * (Y == 1).sum())
+clf = XGBClassifier(max_depth = 3, scale_pos_weight = weights, \
+                n_jobs = 4)
+probabilities = clf.fit(trainX, trainY).predict_proba(testX)
+print('AUPRC = {}'.format(average_precision_score(testY, \
+                                              probabilities[:, 1])))
+
+pickle.dump(clf, open('final_prediction.pkl', 'wb'))      
